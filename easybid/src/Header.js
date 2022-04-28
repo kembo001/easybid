3fb9c60e66c9160c513c626093ddf0806d20e2ff
kembo001 <47574348+kembo001@users.noreply.github.com>
2022-04-28 14:10:26 -0500

    added nav bar and logo

--- easybid/src/Header.js
+++ easybid/src/Header.js
@@ -1,19 +1,24 @@
 import React from 'react'
+import './Header.css'
 
 function Header() {
   return (
     <div className='header'>
-        <h1 className='headerLogo'>Easy Bid</h1>
-        <div className='header_search'>
-          <input className='header_searchInput' type= 'text' />
-      </div>
-    
-    <div className='headerNav'>
-        <div className='headerOption'>
-            <span className='headerOptionLineOne'>Hello Guest</span>
-            <span className='headerOptionLineOne'>Sign In</span>
+        <div className='headerNav'>
+          <nav>
+          <label className='logo'>Easy Bid</label>
+            <ul>
+              <li><a href='#home'>Home</a></li>
+              <li><a href='#auctions'>Auctions</a></li>
+              <li><a href='#account'>Account</a></li>
+              <li><a href='#contact'>Contact</a></li>
+              <li><a href='#aboutUs'>About Us</a></li>
+              <li> <div className='header_search'>
+              <input className='header_searchInput' type= 'text' />
+            </div></li>
+            </ul>
+          </nav>
         </div>
-    </div>
     
     </div>
   )
